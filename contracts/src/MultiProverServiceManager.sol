89ac653f34632d40c12b5a6280c5e1291414f42b
Liao1 <liaoyi1994@gmail.com>
2024-03-20 15:58:43 +0800

    remove dependencies

---
+++ contracts/src/MultiProverServiceManager.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import {ServiceManagerBase} from "eigenlayer-middleware/ServiceManagerBase.sol";
+
+contract MultiProverServiceManager is IMultiProverServiceManager{
+    constructor() {
+    }
+}
