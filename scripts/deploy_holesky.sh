887cfd8af97d55153127a0b84f894a1985c1d4de
exh0115 <165619875+exh0115@users.noreply.github.com>
2024-04-09 10:59:47 +0800

    Update deploy_holesky.sh

--- scripts/deploy_holesky.sh
+++ scripts/deploy_holesky.sh
@@ -18,29 +18,29 @@ function _set_key() {
 	cat /tmp/tmp_set_key > $file
 }
 
+function _set_bool_key() {
+        file=$1
+        key=$2
+        value=$3
+        if [[ $(grep "\\\"$key\\\"" $file) == "" ]]; then
+                echo "key $key not found in $file"
+                return 1
+        fi
+        sed "s!\"$key\": \(true\|false\)!\"$key\": $value!g" $file > /tmp/tmp_set_key
+        cat /tmp/tmp_set_key > $file
+}
+
 
 function update_config() {
-	_set_key config/aggregator.json EcdsaPrivateKey $AGGREGATOR_ECDSA_PRIVATE_KEY
         _set_key config/operator.json EcdsaPrivateKey $OPERATOR_ECDSA_PRIVATE_KEY
 	_set_key config/operator.json BlsPrivateKey $OPERATOR_BLS_PRIVATE_KEY
 	_set_key config/operator.json strategyAddress $STRATEGY_ADDRESS
 
-	_set_key config/aggregator.json EthHttpEndpoint $RPC_URL
-	_set_key config/aggregator.json EthWsEndpoint $RPC_WS
 	_set_key config/operator.json EthRpcUrl $RPC_URL
 	_set_key config/operator.json EthWsUrl $RPC_WS
-
-	_set_key config/aggregator.json Simulation $SIMULATION
-	_set_key config/operator.json simulation $SIMULATION
 	
+	_set_key config/operator.json AggregatorURL $AGGREGATOR_URL
 	_set_key config/operator.json RegistryCoordinatorAddress $REGISTRY_COORDINATOR_ADDRESS
-	_set_key config/aggregator.json AVSRegistryCoordinatorAddress $REGISTRY_COORDINATOR_ADDRESS
-	
-	_set_key config/aggregator.json OperatorStateRetrieverAddress $OPERATOR_STATE_RETRIEVER_ADDRESS
-	
-	_set_key config/aggregator.json MultiProverContractAddress $MULTI_PROVER_CONTRACT_ADDRESS
-	
-	_set_key config/aggregator.json TEELivenessVerifierContractAddress $TEE_LIVENESS_VERIFIER_ADDRESS
 	_set_key config/operator.json TEELivenessVerifierAddr $TEE_LIVENESS_VERIFIER_ADDRESS
 }
 
@@ -53,9 +53,6 @@ function stop() {
 }
 
 source .env
-if [[ "$PRIVATE_KEY" == "" ]]; then
-	export PRIVATE_KEY=$TOPUP_PRIVATE_KEY
-fi
 
 fn=$1
 shift
