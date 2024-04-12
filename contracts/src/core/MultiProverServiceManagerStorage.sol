82d8738eaaba86d529659e8ca7b817c95ec7a7c3
Liao1 <liaoyi1994@gmail.com>
2024-04-12 12:17:01 +0800

    update readme

--- contracts/src/core/MultiProverServiceManagerStorage.sol
+++ contracts/src/core/MultiProverServiceManagerStorage.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
 import {IMultiProverServiceManager} from "../interfaces/IMultiProverServiceManager.sol";
